21e25add5621baa3ac46339fd670679c7899cb61
Raúl Martínez Martínez <raulmartinezmartinez24@gmail.com>
2025-01-24 12:47:14 +0100

    Creacion de Casilla y pasado el test de casilla

---
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Casilla.java
@@ -0,0 +1,33 @@
+package org.iesalandalus.programacion.cuatroenraya.modelo;
+
+import java.util.Objects;
+
+public class Casilla {
+    private Ficha ficha;
+
+    public Casilla() {
+        this.ficha = null;
+    }
+
+    public Ficha getFicha() {
+        return ficha;
+    }
+
+    public void setFicha(Ficha ficha) throws CuatroEnRayaExcepcion {
+        Objects.requireNonNull(ficha,"No se puede poner una ficha nula.");
+
+        if (estaOcupada()) {
+          throw new CuatroEnRayaExcepcion("La casilla ya contiene una ficha.");
+        }
+        this.ficha = ficha;
+    }
+
+    public boolean estaOcupada() {
+        return this.ficha != null;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s",(estaOcupada() ? getFicha() : " " ));
+    }
+}
