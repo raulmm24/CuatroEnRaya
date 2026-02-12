da81170d30535b9bdb0c1b910fa055865e258d40
raulm <raulmartinezmartinez24@gmail.com>
2026-02-12 14:31:17 +0100

    Realización de clase Casilla y pasado su test, y realización de la clase Excepcion

---
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Casilla.java
@@ -0,0 +1,36 @@
+package org.iesalandalus.programacion.cuatroenraya.modelo;
+
+import java.util.Objects;
+
+public class Casilla {
+
+    private Ficha ficha;
+
+    public Casilla() {
+        this.ficha = null;
+    }
+
+    public Ficha getFicha() {
+        return ficha;
+    }
+
+    public void setFicha(Ficha ficha) throws CuatroEnRayaExcepcion {
+        Objects.requireNonNull(ficha, "No se puede poner una ficha nula.");
+
+        if (estaOcupada()) {
+            throw new CuatroEnRayaExcepcion("La casilla ya contiene una ficha.");
+        }
+
+        this.ficha = ficha;
+    }
+
+    public boolean estaOcupada() {
+        return ficha != null;
+    }
+
+    @Override
+    public String toString() {
+        // Devuelve " " si es null, o el toString de la ficha ("A" o "V") si está ocupada
+        return (!estaOcupada()) ? " " : ficha.toString();
+    }
+}
---
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/CuatroEnRayaExcepcion.java
@@ -0,0 +1,7 @@
+package org.iesalandalus.programacion.cuatroenraya.modelo;
+
+public class CuatroEnRayaExcepcion extends RuntimeException {
+    public CuatroEnRayaExcepcion(String message) {
+        super(message);
+    }
+}
